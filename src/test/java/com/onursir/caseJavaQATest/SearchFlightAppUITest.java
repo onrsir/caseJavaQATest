1268337ea3e9552350245c16eb58191f02e74f7d
onur sir <oonursir@gmail.com>
2024-02-03 18:13:00 +0300

    recent changes

--- src/test/java/com/onursir/caseJavaQATest/SearchFlightAppUITest.java
+++ src/test/java/com/onursir/caseJavaQATest/SearchFlightAppUITest.java
@@ -9,8 +9,6 @@ public class SearchFlightAppUITest {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
 
-            String noFlight = "uçuş bulunmuyor";
-
             page.navigate("https://flights-app.pages.dev/");
             page.waitForLoadState();
 
@@ -19,16 +17,17 @@ public class SearchFlightAppUITest {
             selectCity(page, "headlessui-combobox-button-:R1ahlla:", "Istanbul");
 
             // Check if the application allows the same "From" and "To" values
-            boolean isSameValueAllowed = page.isVisible(noFlight);
+            boolean isSameValueAllowed = page.isVisible("text=uçuş bulunmuyor");
+
             if (isSameValueAllowed) {
                 System.out.println("Bug Detected: The same values can be entered for 'From' and 'To'.");
+
             } else {
                 System.out.println("Test Passed: Cannot enter the same values for 'From' and 'To'.");
             }
             browser.close();
         }
     }
-
     private static void selectCity(Page page, String buttonId, String city) {
         page.click(String.format("xpath=//button[@id='%s']", buttonId));
         page.click(String.format("text=%s", city));
