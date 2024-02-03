0b75eba66b58fb0ab11b59643faca3b24ce48edb
onur sir <oonursir@gmail.com>
2024-02-03 06:15:03 +0300

    Initial commit

---
+++ src/main/java/com/onursir/caseJavaQATest/CaseJavaQaTestApplication.java
@@ -0,0 +1,13 @@
+package com.onursir.caseJavaQATest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseJavaQaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseJavaQaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onursir/caseJavaQATest/CaseJavaQaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onursir.caseJavaQATest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseJavaQaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
